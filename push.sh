7b7a911207cd7a991289a13204708968a5179ca6
sfx <songfangxi2012@163.com>
2022-10-10 09:30:14 +0800

    update push.sh

--- push.sh
+++ push.sh
@@ -7,5 +7,6 @@ br=`obtain_git_branch`
 echo Current git branch is $br
 git remote add github git@github.com:sophimp/vim-init.git
 git remote add skywind git@github.com:skywind3000/vim-init.git
+git remote set-url origin git@gitee.com:sophimp/vim-init.git
 git push origin $br
 git push github $br
